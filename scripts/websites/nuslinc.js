c55e3a09665dcfe774e2792a7bb66b4753bafdf6
Bobby Lin <bobbylinjh@gmail.com>
2015-12-14 22:53:03 +0800

    Added search() in nuslinc.js

--- scripts/websites/nuslinc.js
+++ scripts/websites/nuslinc.js
@@ -2,3 +2,11 @@
  * Created on: 14/12/15
  *     Author: Bobby Lin
  */
+
+function search(keywords) {
+    var front = "http://linc.nus.edu.sg/search/Y?SEARCH=";
+    var end = "&searchscope=16&SORT=D&submit=Submit";
+    var url = front + keywords + end;
+    var win = window.open(url, '_blank');
+    win.focus();
+}
