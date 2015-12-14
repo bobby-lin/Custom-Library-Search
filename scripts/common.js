338ce1beaf146b5290e9db4bfc0d2cbda7bbb591
Bobby Lin <bobbylinjh@gmail.com>
2015-12-14 21:45:37 +0800

    Added executeSearch() in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -2,3 +2,23 @@
  * Created on: 14/12/15
  *     Author: Bobby Lin
  */
+
+function executeSearch() {
+    var keywords = getKeywordsValue();
+    var website = getWebsiteValue();
+    console.log(website + " " + keywords);
+    clearKeywordsValue();
+}
+
+function getKeywordsValue() {
+    return document.getElementById('keywords').value;
+}
+
+function getWebsiteValue() {
+    var e = document.getElementById('website');
+    return e.options[e.selectedIndex].text;
+}
+
+function clearKeywordsValue() {
+    document.getElementById('keywords').value ='';
+}
