a4f8ad8cb219e159c1f0a934fa50dc595736a092
Bobby Lin <bobbylinjh@gmail.com>
2015-12-14 22:35:35 +0800

    Added search() in nlb.js

--- scripts/websites/nlb.js
+++ scripts/websites/nlb.js
@@ -2,3 +2,11 @@
  * Created on: 14/12/15
  *     Author: Bobby Lin
  */
+
+function search(keywords) {
+    var front = "http://catalogue.nlb.gov.sg/cgi-bin/spydus.exe/ENQ/EXPNOS/BIBENQ?ENTRY=";
+    var end = "&ENTRY_NAME=BS&ENTRY_TYPE=K&SORTS=DTE.DATE1.DESC%5DHBT.SOVR";
+    var url = front + keywords + end;
+    var win = window.open(url, '_blank');
+    win.focus();
+}
