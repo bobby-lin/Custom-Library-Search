3a048a7abd79365e13f2636633b6656a380aaec3
Bobby Lin <bobbylinjh@gmail.com>
2015-12-14 22:36:10 +0800

    Added executeSearch() and load() in executeSearch.js

--- scripts/executeSearch.js
+++ scripts/executeSearch.js
@@ -2,3 +2,15 @@
  * Created on: 14/12/15
  *     Author: Bobby Lin
  */
+
+function executeSearch(keywords,website) {
+    alert("Inside executeSearch");
+    var path = "scripts/websites/" + website + ".js";
+    load(keywords, path);
+}
+
+function load(keywords, path) {
+    $.getScript(path, function () {
+        search(keywords);
+    });
+}
